452d1659d30f784e4a8351f44ced6517820bf03b
Aleesha Mufthi <aleeshamufthi@gmail.com>
2025-02-18 13:58:20 +0530

    lAST commit

--- src/config/corsConnection.js
+++ src/config/corsConnection.js
@@ -2,7 +2,7 @@ const corsConnection=()=>{
 
     console.log("LOCALE_URL:", process.env.LOCALE_URL); 
     return {
-        origin: process.env.LOCALE_URL, // Allow all origins 
+        origin: "*",
         methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
         credentials: true // Include cookies in CORS requests
     }
